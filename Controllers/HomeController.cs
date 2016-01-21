436f84dcce72fe849401986088b3a02e35fcca0a
Shrutika Mendhe Patil <shrutika.mendhe@click2cloud.net>
2016-01-21 12:17:47 +0530

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,8 +18,9 @@ namespace MvcSample.Web
             //Retrive Parameters from Environment Variables
             string userName = Environment.GetEnvironmentVariable("MONGODB_USER");
             string password = Environment.GetEnvironmentVariable("MONGODB_PASSWORD");
-            string server = "172.30.56.23"; 
             //string server = Environment.GetEnvironmentVariable("DATABASE_SERVICE_NAME");
+            //TODO: Provide ip of mongo db server 
+            string server = "172.30.56.23"; 
             string databaseName = Environment.GetEnvironmentVariable("MONGODB_DATABASE");
 
 
